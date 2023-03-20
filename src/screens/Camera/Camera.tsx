50a7bb9ed63c934f2ab6b2becf2a8250d40bb30b
eborda <eborda@hexagonconsulting.net>
2023-03-20 17:24:28 -0300

    Taking snapshot

--- src/screens/Camera/Camera.tsx
+++ src/screens/Camera/Camera.tsx
@@ -1,12 +1,14 @@
 import { useIsFocused } from "@react-navigation/native";
 import React, { useEffect, useRef, useState } from "react";
+import { TouchableOpacity } from "react-native";
 import { 
+	ActivityIndicator,
+	Image,
 	Text, 
-	TouchableOpacity, 
 	useColorScheme, 
 	View
 } from "react-native";
-import { Camera, useCameraDevices } from "react-native-vision-camera";
+import { Camera, PhotoFile, useCameraDevices } from "react-native-vision-camera";
 
 
 import {bgColor, colors, fonts, layout, m, mx, my} from '../../styles/styles';
@@ -44,8 +46,10 @@ const CameraScreen: React.FC = () => {
 	const devices = useCameraDevices();
 	const camera = useRef<Camera>(null);
 
+	const [loading, setLoading] = useState(false);
 	const [showCamera, setShowCamera] = useState<boolean>(false);
 	const [alertMessage, setAlertMessage] = useState<string>('');
+	const [currentPhoto, setCurrentPhoto] = useState<PhotoFile | null>(null);
 
 	const openCamera = async () => {
 		console.log('Checking devices...')
@@ -83,11 +87,32 @@ const CameraScreen: React.FC = () => {
 		}
 	};
 
-	const takeSnapshot = async () => {
-		return await camera.current?.takeSnapshot({
+	const takeSnapshot = () => {
+		console.log('Taking photo..');
+		setLoading(true);
+
+		console.log('Camera:', !!camera.current);
+		camera.current?.takeSnapshot({
 			quality: 85,
 			skipMetadata: true,
+		}).then( photoTaken => {
+			if (photoTaken) {
+				setCurrentPhoto(photoTaken);
+				setShowCamera(false);
+			}
+			else {
+				setAlertMessage('Hubo un problema al intentar tomar la foto.');
+			}
+		}).catch(reason => {
+			console.log(reason);
+		}).finally(() => {
+			setLoading(false);
 		});
+
+	}
+
+	const testAction = () => {
+		console.log('Tested');
 	}
 
 	useEffect(() => {
@@ -109,24 +134,55 @@ const CameraScreen: React.FC = () => {
 						isActive={true} style={{flex: 1}} photo={true}
 					/>
 					<TouchableOpacity 
-						onPress={() => console.log('Taking photo..')}
-					><View 
-					style={{
-						backgroundColor: 'white',
-						borderRadius: 32,
-						position: 'absolute',
-						height: 64,
-						width: 64,
-						bottom: 16,
-						alignSelf: 'center'
-					}}
-					/></TouchableOpacity>
+						onPress={takeSnapshot}
+						style={{
+							position: 'absolute',
+							bottom: 16,
+							alignSelf: 'center',
+						}}
+					>
+						<View 
+							style={{
+								backgroundColor: 'white',
+								borderRadius: 36,
+							
+								height: 72,
+								width: 72,
+								bottom: 16,
+								alignSelf: 'center',
+								borderWidth: 8,
+								borderColor: '#FFF8'
+							}}
+						/>
+					</TouchableOpacity>
 				</>
+				: null
+			}
+			{
+				currentPhoto
+				? <Image 
+					source={{uri: 'file://' + currentPhoto.path}}
+					style={{
+						flex: 1,
+						borderRadius: 20,
+						backgroundColor: colors[colorScheme].background,
+						padding: 32,
+					}}
+				/>
 				: null
 			}
 			{
 				alertMessage
 				? <PopUp message={alertMessage} /> : null
+			}
+			{
+				loading
+				? <ActivityIndicator size={128} color={'white'} style={{
+					position: 'absolute',
+					alignSelf: 'center',
+					top: '40%'
+				}}/>
+				: null
 			}
 		</View>
 	);
